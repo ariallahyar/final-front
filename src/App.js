ce9584e5e11e4818be14a25a342d237a02b05995
Arianna Allahyar <anallahyar@gmail.com>
2022-06-07 10:43:34 +0200

    Development mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Map from "./components/Map";
+import mockData from "./mock-data.json";
+
+const markers = mockData.results; // for development
 
 const App = () => {
-  const [markers, setMarkers] = useState([]);
-  
-  useEffect(() => {
-    fetch("https://arieats.herokuapp.com/places")
-      .then((res) => res.json())
-      .then((data) => setMarkers(data.results))
-      .catch((err) => console.log(err));
-  }, []);
+  // const [markers, setMarkers] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://arieats.herokuapp.com/places")
+  //     .then((res) => res.json())
+  //     .then((data) => setMarkers(data.results))
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return <Map markers={markers} />;
 };
