77bc956c841b5c9698b4e06d084337b5ac05aa5f
Arianna Allahyar <anallahyar@gmail.com>
2022-06-09 13:57:50 +0200

    Header and footer components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1>Header</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/community">Community Curated</Link>
+        <Link to="/society">Supper Society</Link>
+      </nav>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return <footer>Footer</footer>;
+};
+
 const Layout = () => {
   return (
     <>
-      <header>
-        <h1>Header</h1>
-        <nav>
-          <Link to="/">Home</Link>{" "}
-          <Link to="/community">Community Curated</Link>{" "}
-          <Link to="/society">Supper Society</Link>{" "}
-        </nav>
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
-      <footer>Footer</footer>
+      <Footer />
     </>
   );
 };
