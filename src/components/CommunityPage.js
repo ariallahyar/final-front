de841c4eedd8549b4d45ceec179709c8f4f355ef
Arianna Allahyar <anallahyar@gmail.com>
2022-06-10 11:21:15 +0200

    Fetch recommendations from backend

--- src/components/CommunityPage.js
+++ src/components/CommunityPage.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommunityForm from "./CommunityForm";
 
-import entries from "../mock-entries.json";
-
 const CommunityPage = () => {
+  const [recommendations, setRecommendations] = useState([]);
+
+  useEffect(() => {
+    fetch("https://arieats.herokuapp.com/recommendations")
+      .then((res) => res.json())
+      .then((data) => setRecommendations(data.results))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <h2>Community curated</h2>
       <div className="container-community">
         <section>
-          {entries.results.map(({ nameOfPlace, city, comment }) => {
+          {recommendations.map(({ nameOfPlace, city, comment, website }) => {
             return (
               <article>
                 <h4>{nameOfPlace}</h4>
                 <p>{city}</p>
                 <p>{comment}</p>
+                <a href={website} target={"_blank"} rel="noreferrer">
+                  {website}
+                </a>
               </article>
             );
           })}
