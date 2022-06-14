98fb879c4623fb128324c56d472179e9428fd656
Arianna Allahyar <anallahyar@gmail.com>
2022-06-14 16:45:31 +0200

    Revised API response

---
+++ src/assets/icons.js
@@ -0,0 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot as address, faHome as homepage } from "@fortawesome/free-solid-svg-icons";
+
+export const location = <FontAwesomeIcon icon={address} />;
+export const website = <FontAwesomeIcon icon={homepage} />;
