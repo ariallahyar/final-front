a5207f7c330576c373a22eed45df0362d8f47e21
Arianna Allahyar <anallahyar@gmail.com>
2022-06-22 00:02:53 +0200

    Small styling adjustments

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -3,33 +3,42 @@ import { profileIcon } from "../assets/icons";
 import Login from "./Login";
 import { logout, deleteAccount } from "../api/user-auth";
 import { SubmitButton } from "./Button";
+import styled from "styled-components";
+
+const Container = styled.section`
+  max-width: 400px;
+`;
 
 const ProfilePage = () => {
-  const [authorized, setAuthorized] = useState(localStorage.getItem("Token"));
-  // const [token, setToken] = useState(() => localStorage.getItem("Token"));
+  const [authorized, setAuthorized] = useState(localStorage.getItem("Token")); 
 
-  const handleOnSubmit = (event) => {
+  const deleteOnSubmit = (event) => {
     event.preventDefault();
     deleteAccount(() => setAuthorized(localStorage.getItem("Token")));
   };
 
-  const handleOnSubmitLogout = (event) => {
+  const logoutOnSubmit = (event) => {
     event.preventDefault();
     logout(() => setAuthorized(localStorage.getItem("Token")));
   };
 
-  if (!authorized) return <Login setAuthorized={setAuthorized} />;
+  if (!authorized)
+    return (
+      <Container>
+        <Login setAuthorized={setAuthorized} />
+      </Container>
+    );
 
   return (
-    <section>
+    <Container>
       <h2>{profileIcon} Welcome to your profile page</h2>
-      <form onSubmit={handleOnSubmitLogout}>
+      <form onSubmit={logoutOnSubmit}>
         <SubmitButton dark label="Log Out" />
       </form>
-      <form onSubmit={handleOnSubmit}>
+      <form onSubmit={deleteOnSubmit}>
         <SubmitButton label="Delete Account" />
       </form>
-    </section>
+    </Container>
   );
 };
 
